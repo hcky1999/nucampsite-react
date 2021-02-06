37dc21963a84d78f91232932243b639167bcbeb4
hcky1999 <heckladiv@icloud.com>
2021-02-05 23:48:33 -0800

    Added Exercise Lesson: Header and Footer

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,60 +1,34 @@
-import React, {Component} from 'react';
-import { Card, CardImgOverlay, CardTitle, CardImg } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Directory extends Component{
-
-    render(){
-        const directory = this.props.campsites.map(campsite => {
-            return(
-                // need to add unique id
-                <div key={campsite.id} className="col-md-5 m-1">
-                   <Card onClick={() => this.props.onClick(campsite.id)}>    
-                        <CardImg width="100%" src={campsite.image} alt = {campsite.name} />
-                        <CardImgOverlay>
-                             <CardTitle> {campsite.name} </CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-                // use a jsx div
-            );
-        });
-        
-        return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
-                
-            </div>
-        );
-
-    }
-
-
+function RenderDirectoryItem({campsite, onClick}) {
+    return (
+        <Card onClick={() => onClick(campsite.id)}>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+                <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
 }
 
-//concept of props in components, and how to pass props from one component to another.
-// to pass data to component A to component B
+function Directory(props) {
 
-// class ExampleParentComponent extends Component {
-//     constructor(props){
-//         super(props); 
-//         this.state = {
-//             number: 333
-//         }
-//     }
-//     render() {
-//         return <ExampleChildComponent number = {this.state.number} greetings = "Hello World" />;
-//     }
-// }
+    const directory = props.campsites.map(campsite => {
+        return (
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+            </div>
+        );
+    });
 
-// class ExampleChildComponent extends Component {
-//     render() {
-//         return <div>{this.props.number} {this.props.greetings}</div>
-//     }
-// }
-
-
-
+    return (
+        <div className="container">
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
+}
 
 export default Directory;
